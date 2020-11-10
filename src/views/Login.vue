65ec52bc84ca255748afebd1851ebdddd9bb69f3
b3an5 <beantheo@gmail.com>
2020-11-10 13:12:13 -0700

    login page

---
+++ src/views/Login.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class='login'>
+        <b-jumbotron 
+                        header="vue.js chat"
+                        lead="powered by channels sdk and bootstrap-vue"
+                        bg-varient='info'
+                        text-variant="white"
+        >
+            <p>For more info visit this website</p>
+            <b-btn target="_blank" href="https://pusher.com/channels">More Info</b-btn>
+        </b-jumbotron>
+        <b-container>
+      <b-row>
+        <b-col lg="4" md="3"></b-col>
+        <b-col lg="4" md="6">
+          <LoginForm />
+        </b-col>
+        <b-col lg="4" md="3"></b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import LoginForm from '@/components/LoginForm.vue'
+
+export default {
+  name: 'login',
+  components: {
+    LoginForm
+  }
+}
+</script>
