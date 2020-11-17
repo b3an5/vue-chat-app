f12a9974e4ce99eb136ff006f8907f1576b1379f
b3an5 <beantheo@gmail.com>
2020-11-17 13:17:53 -0700

    message list

---
+++ src/components/MessageList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="message-list">
+    <h4>Messages</h4>
+    <hr>
+    <div id="chat-messages" class="message-group" v-chat-scroll="{smooth: true}">
+      <div class="message" v-for="(message, index) in messages" :key="index">
+        <div class="clearfix">
+          <h4 class="message-title">{{ message.name }}</h4>
+          <small class="text-muted float-right">@{{ message.username }}</small>
+        </div>
+        <p class="message-text">
+          {{ message.text }}
+        </p>
+        <div class="clearfix">
+          <small class="text-muted float-right">{{ message.date }}</small>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: 'message-list',
+  computed: {
+    ...mapState([
+      'messages',
+    ])
+  }
+}
+</script>
